812ee925d28f0b547012c1a5b3555644107d4992
snielsen221b <sophianielsen221b@gmail.com>
2017-09-24 14:50:59 -0400

    2D Scanner working, data collected

--- Lab_2/TwoDScanner.m
+++ Lab_2/TwoDScanner.m
@@ -9,21 +9,22 @@ arduino_board = arduino('COM9', 'Uno', 'Libraries', 'Servo');
 
 % set up angle step and define number of 'ticks' per sweep of the servo
 % angle bounds are 0 and 1
-x_angle_step = .1;
+x_angle_step = .05;
 x_angle_ticks = 1/x_angle_step;
-y_angle_step = .1;
-y_angle_ticks = 1/y_angle_step;
+y_angle_ticks = 1
 
 % Create servo objects and servo position array
 x_servo = servo(arduino_board, 'D4');
 x_servo_positions = zeros(x_angle_ticks, 1);
-%y_servo = servo(arduino_Board, 'D5');
+y_servo = servo(arduino_board, 'D5');
 
 % Define IR pin and IR data array
 IR_pin = 'A0';
 IR_data = zeros(x_angle_ticks, y_angle_ticks);
 
 
+% Sets y_servo straight on
+writePosition(y_servo, 1)
 % Sweeps servo from 0 to 180 deg
 for x_angle = 0:x_angle_step:1
     writePosition(x_servo, x_angle);
@@ -31,19 +32,17 @@ for x_angle = 0:x_angle_step:1
     x_current_position = x_current_position*180;
     i = int8(x_angle*(1/x_angle_step) + 1)
     
-    %for y_angle = 0:y_angle_step:1
-    %    writePositon(y_servo, y_angle);
-    %    y_current_position = readPosition(y_servo);
-     %   y_current_position = y_current_position*180
-        j = 1;
+    j = 1;
 
-        % store servo position in the vector servo_1_positions
-        x_servo_positions(i) = x_current_position;
+    % store servo position in the vector servo_1_positions
+    x_servo_positions(i) = x_current_position;
 
-        % read voltage from IR_pin and store it
-        IR_data(i, j) = readVoltage(arduino_board, IR_pin)
-    
+    % read voltage from IR_pin and store it
+    IR_data(i, j) = readVoltage(arduino_board, IR_pin)
+
     pause(1);
 end
 
+csvwrite('2D_IR.csv', IR_data);
+csvwrite('2D_xpos.csv', x_servo_positions);
 clear arduino_board servo_1;
